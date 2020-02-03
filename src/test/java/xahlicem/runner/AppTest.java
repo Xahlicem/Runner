fcbbe66cf29ffd8c03bcb71e0399b405d8700928
Xahlicem <xahlicem@gmail.com>
2020-02-02 23:13:16 -0800

    Simplified pom.

--- src/test/java/xahlicem/runner/AppTest.java
+++ src/test/java/xahlicem/runner/AppTest.java
@@ -1,8 +1,8 @@
 package xahlicem.runner;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
